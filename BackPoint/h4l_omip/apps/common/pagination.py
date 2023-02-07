1c8c17d628af15ed6ea1c18a16295533400ac4ce
souno <souno@qq.com>
2023-02-07 16:40:42 +0800

    :rocket: feature: 完善基础功能架构

--- BackPoint/h4l_omip/apps/common/pagination.py
+++ BackPoint/h4l_omip/apps/common/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class DataCenterPageNumberPagination(PageNumberPagination):
+class H4LPageNumberPagination(PageNumberPagination):
     page_query_param = 'pageNo'
     page_size_query_param = 'pageSize'  # ?pageNo=xx&pageSize=??
     max_page_size = None  # 最大页数设置
