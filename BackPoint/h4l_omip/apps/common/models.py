c092608cfc55be59bd41af23c5005a6bf4122821
souno <souno@qq.com>
2023-02-15 17:03:16 +0800

    :rocket: feature: 完善基础功能架构

--- BackPoint/h4l_omip/apps/common/models.py
+++ BackPoint/h4l_omip/apps/common/models.py
@@ -21,7 +21,8 @@ class H4LBaseModel(models.Model):
     # id = models.CharField('id', primary_key=True, max_length=25, default=non_secure_generate(size=21))
     is_active = models.BooleanField(_('激活状态'), default=True, help_text='状态说明字段')
     update_datetime = models.DateTimeField(_("修改时间"), auto_now=True, null=True, blank=True, help_text="修改时间")
-    create_datetime = models.DateTimeField(_("创建时间"), auto_now_add=True, null=True, blank=True, help_text="创建时间")
+    create_datetime = models.DateTimeField(_("创建时间"), auto_now_add=True, null=True, blank=True,
+                                           help_text="创建时间")
 
     class Meta:
         abstract = True
@@ -34,22 +35,19 @@ def get_all_models_objects(model_name=None):
     获取所有 models 对象
     :return: {}
     """
-    settings.ALL_MODELS_OBJECTS = {}
-    if not settings.ALL_MODELS_OBJECTS:
+    if not hasattr(settings, 'ALL_MODELS_OBJECTS') or not settings.ALL_MODELS_OBJECTS:
+        settings.ALL_MODELS_OBJECTS = {}
         all_models = apps.get_models()
-        for item in list(all_models):
+        for item in all_models:
             table = {
                 "tableName": item._meta.verbose_name,
                 "table": item.__name__,
-                "tableFields": []
+                "tableFields": [
+                    {"title": field.verbose_name, "field": field.name} for field in item._meta.fields
+                ]
             }
-            for field in item._meta.fields:
-                fields = {
-                    "title": field.verbose_name,
-                    "field": field.name
-                }
-                table['tableFields'].append(fields)
-            settings.ALL_MODELS_OBJECTS.setdefault(item.__name__, {"table": table, "object": item})
+            settings.ALL_MODELS_OBJECTS[item.__name__] = {"table": table, "object": item}
+
     if model_name:
-        return settings.ALL_MODELS_OBJECTS[model_name] or {}
-    return settings.ALL_MODELS_OBJECTS or {}
+        return settings.ALL_MODELS_OBJECTS.get(model_name, {})
+    return settings.ALL_MODELS_OBJECTS
