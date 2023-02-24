54010316506d3fd1a7d48cdc1cba8a312cdfc42d
souno <souno@qq.com>
2023-02-24 10:34:48 +0800

    :rocket: feature: 初步完成sql拼接，现差年龄处理。

--- BackPoint/h4l_omip/apps/drgs/api_views.py
+++ BackPoint/h4l_omip/apps/drgs/api_views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import SingleDisease, StaResults
 from .serializers import SingleDiseaseSerializer, StaResultsSerializer
 
@@ -6,6 +9,18 @@ from .serializers import SingleDiseaseSerializer, StaResultsSerializer
 class SingleDiseaseViewSet(viewsets.ModelViewSet):
     queryset = SingleDisease.objects.all()
     serializer_class = SingleDiseaseSerializer
+
+    @action(detail=True, methods=["GET"])
+    def query(self, request, *args, **kwargs):
+        if not all([request.GET.get("start_date"), request.GET.get("end_date")]):
+            raise Exception('start_date 和 end_date 必须提供！')
+        return Response(
+            self.get_object().fetch(
+                start_date=request.GET.get("start_date"),
+                end_date=request.GET.get("end_date"),
+                user=request.user
+            )
+        )
 
 
 class StaResultsViewSet(viewsets.ModelViewSet):
